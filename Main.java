01f01a5d3ed97b7755e5cc290d54fe4f7a4636f4
ChristopherBrito <britc30@farmingdale.edu>
2025-05-13 20:27:19 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+	public static void main(String[] args) {
+		Point<String> strPoint = new Point<String>("Anna", " Banana");
+		System.out.println(strPoint);
+		Point<Number> pie = new Point<Number>(3.14, 2.17);
+		System.out.println(pie);
+		Point huh = new Point("Hi"," Sir");
+		System.out.println(huh);
+		Point heh = new Point(32," Sir");
+		System.out.println(heh);
+	}
+}
+	
